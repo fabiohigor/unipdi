c504353172a8101af55e0b3d2a5d0c5b4d44241a
Fabio <fabioec.2010@gmail.com>
2025-11-15 12:53:40 -0400

    Finalizando aula 08

--- unipdi-backend/src/main/java/br/com/unipds/unipdi/config/CorsConfig.java
+++ unipdi-backend/src/main/java/br/com/unipds/unipdi/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173") // endereço do frontend
+                        .allowedOrigins("*") // endereço do frontend
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
